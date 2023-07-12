3368721e24158ed5e23086991be14f6ffab6f7f9
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-12 13:57:35 +0300

    task_day4

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first num :-");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("enter the operations:-");
+            string ope = Console.ReadLine();
+            Console.WriteLine("enter the second num :-");
+            float num2 = float.Parse(Console.ReadLine());
+            operations opt = new operations();
+            switch (ope)
+            {
+                case "+":
+                    Console.WriteLine($"{num1} + {num2} = {opt.sum(num1,num2)}");
+                    break;
+                    
+                case "-":
+                    Console.WriteLine($"{num1} - {num2} = {opt.sub(num1, num2)}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{num1} * {num2} = {opt.mul(num1, num2)}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{num1} / {num2} = {opt.div(num1, num2)}");
+                    break;
+                case "%":
+                    Console.WriteLine($"{num1} % {num2} = {opt.rem(num1, num2)}");
+                    break;
+            }
+            
+
+        }
+
+    }
+
+    class operations
+    {
+        public float sum (float N1 , float N2)
+        {
+            return N1 + N2;
+        }
+
+        public float sub(float N1, float N2)
+        {
+            return N1 - N2;
+        }
+
+        public float mul(float N1, float N2)
+        {
+            return N1 * N2;
+        }
+
+        public float div(float N1, float N2)
+        {
+            return N1 / N2;
+        }
+
+        public float rem(float N1, float N2)
+        {
+            return N1 % N2;
+        }
+    }
+}
